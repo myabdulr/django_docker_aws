74aa0ac0a6fa3bd83d89078975c24d0ddcbf9f49
Ubuntu <ubuntu@ip-172-31-13-80.ec2.internal>
2024-09-30 20:39:22 +0000

    Updated allowed

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rd_q8=#5%)xlhl8!b8o410apqror1(3^2j6m3jr#xr&!+!8%84
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-44-200-15-35.compute-1.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-44-215-249-23.compute-1.amazonaws.com','ec2-44-200-15-35.compute-1.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
